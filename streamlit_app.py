855f862cf3da55f1d902e72645a6fe03e7eac25d
eko1788 <eko1788@gmail.com>
2026-09-09 17:58:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,14 @@ import streamlit as st
 st.title("This is a title")
 st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 st.title("Dashboard", icon=":material/dashboard:")
+
+import streamlit as st
+
+st.header("_Streamlit_ is :blue[cool] :sunglasses:")
+st.header("This is a header with a divider", divider="gray")
+st.header("These headers have rotating dividers", divider=True)
+st.header("One", divider=True)
+st.header("Two", divider=True)
+st.header("Three", divider=True)
+st.header("Four", divider=True)
+st.header("Header with an icon", icon=":material/home:")
